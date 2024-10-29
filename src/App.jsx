dc5706e8da204270817385e325323184e04e79b8
1RISHABH-TRIAPTHI <rishabhtripathi485551@gmail.com>
2024-10-29 21:12:08 +0530

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,5 @@
 import './App.css'
 import ConTrol_Pathwith_Com from './Component/controlPathWithCom'
-import Navbar from './Component/Navbar_Based/Navbar'
-
-
 function App() {  
   return (
     <>
--- src/Component/controlPathWithCom.jsx
+++ src/Component/controlPathWithCom.jsx
@@ -1,13 +1,14 @@
 import React from "react";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import {  createHashRouter, RouterProvider } from "react-router-dom";
 import Navbar from "./Navbar_Based/Navbar";
 import Category from "./Category";
 import HomePage from "./Navbar_Based/HomePage";
 import News_Grid from "./news_Component/News_Grid";
 import topNewsGrid from "./news_Component/topNewsGrid";
 import EveryNewsGrid from "./news_Component/EveryNewsGrid";
+import NotFoundPage from "./Errror_Page/notFoundPage";
 function ConTrol_Pathwith_Com() {
-  const router = createBrowserRouter([
+  const router = createHashRouter([
     {
       path: "/",
       Component: Navbar,
@@ -32,6 +33,10 @@ function ConTrol_Pathwith_Com() {
           path:'every',
           Component:EveryNewsGrid
         },
+        {
+          path:'*',
+          Component:NotFoundPage
+        }
       ],
     },
   ]);
