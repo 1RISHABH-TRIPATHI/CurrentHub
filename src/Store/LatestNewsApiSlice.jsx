8dcc3da4c947400b69a304e9c8ad9a16fbf3e4d5
1RISHABH-TRIAPTHI <rishabhtripathi485551@gmail.com>
2024-11-06 22:19:38 +0530

    update latest env

--- src/Store/LatestNewsApiSlice.jsx
+++ src/Store/LatestNewsApiSlice.jsx
@@ -4,8 +4,8 @@ export const API = createApi({
   endpoints: (builder) => ({
     getTasks: builder.query({
       query: ({ page, lang, country, Category, Priority }) => {
-        
-        let url = `latest?apikey=pub_53510d0f2b001d47290025f4967c278994445`;
+        const k1= process.env.VITE_API_URL1;
+        let url = `latest?apikey=${k1}`;
         if (page) url += `&page=${page}`;
         if (country != "all") url += `&country=${country}`;
         if (lang) url += `&language=${lang}`;
@@ -16,13 +16,13 @@ export const API = createApi({
     }),
     getTopHeading: builder.query({
       query: ({lang,country}) => ({
-        url: `https://newsapi.org/v2/top-headlines?country=${country}&apiKey=7ce0179279dc4b33af4a63052049287f&pageSize=100&language=${lang}`,
+        url: `https://newsapi.org/v2/top-headlines?country=${country}&apiKey=${process.env.VITE_API_URL2}&pageSize=100&language=${lang}`,
         method: "GET",
       }),
     }),
     getEveryNews:builder.query({
       query:({lang,page,search})=>({
-        url:`https://newsapi.org/v2/everything?q=${search ? search :'a'}&apiKey=7ce0179279dc4b33af4a63052049287f&pageSize=50&languge=${lang}&page=${page}`,
+        url:`https://newsapi.org/v2/everything?q=${search ? search :'a'}&apiKey=${process.env.VITE_API_URL2}&pageSize=50&languge=${lang}&page=${page}`,
         method:'GET'
       })
     })
